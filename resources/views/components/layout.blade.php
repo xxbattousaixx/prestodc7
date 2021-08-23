233c4ce73bb7c3ade7c6b0d3f548a6d44440e508
Donato Carrassi <dcarrassi@outlook.com>
2021-08-23 17:57:11 +0200

    navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
     <title>Document</title>
 </head>
 <body>
@@ -11,4 +12,5 @@
         {{$slot}}
     <x-footer />
 </body>
+<script src="js/app.js"></script>
 </html>
