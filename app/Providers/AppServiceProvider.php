5ccc61b918a1e630e9ec85509493be40de4aeb4f
Gabriele  Tomba <gabriele.tomba@gmail.com>
2021-08-25 18:35:15 +0200

    Progetto clone realizzato

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+
 use Illuminate\Pagination\Paginator;
 
 use App\Models\Category;
@@ -29,6 +30,5 @@ class AppServiceProvider extends ServiceProvider
         $categories = Category::all();
         View::share(compact('categories'));
         Paginator::useBootstrap();
-
     }
 }
